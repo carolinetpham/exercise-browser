28d7d30252bee2fcc91edf3180c6777ad626209f
carolinetpham <carolinetpham@outlook.com>
2024-05-17 15:59:35 -0400

    installing bootstrap and added classnames

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -22,8 +22,8 @@ export default function SearchBar() {
             <nav className="navbar navbar-light bg-light">
                 <div className="container-fluid">
                     <form className="d-flex" onSubmit={handleSubmit}>
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={input} onChange={(e) => setInput(e.target.value)}></input>
-                        <button className="btn btn-outline-success" type="submit">Search</button>
+                        <input className="form-control me-2 search-bar" type="search" placeholder="Search" aria-label="Search" value={input} onChange={(e) => setInput(e.target.value)}></input>
+                        <button className="btn btn-outline-success search-button" type="submit">Search</button>
                     </form>
                 </div>
             </nav>
@@ -31,7 +31,6 @@ export default function SearchBar() {
                 {exercises.map((exercise, index) => (
                     <div key={index}>
                         <h3>Name: {exercise.name}</h3>
-                        <h3>Type: {exercise.type}</h3>
                         <h3>Muscle: {exercise.muscle}</h3>
                         <h3>Equipment: {exercise.equipment}</h3>
                         <h3>Difficulty: {exercise.difficulty}</h3>
